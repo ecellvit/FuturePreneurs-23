9b1968252f39d2f6347da29e92383077f9534d01
Pratyush <connect2pratyush@gmail.com>
2023-10-05 16:19:32 +0530

    samples

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className='m-40'>
       FuturePreneurs 2023
     </div>
   )
---
+++ pages/joinTeam/index.js
@@ -0,0 +1,5 @@
+export default function JoinTeam(){
+  return (
+    "Join Team"
+  )
+}
