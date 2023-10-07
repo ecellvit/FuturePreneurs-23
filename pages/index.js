86f759b8a7c72ea285e432578e44233e7d3a3d8f
kv8gh <vyaskaran1409@gmail.com>
2023-10-07 23:21:35 +0530

    leaderdash

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import LeaderDashboardCards from './LeaderDashboardCards'
+import LeaderDashboard from './LeaderDashboard'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='m-40'>
-      FuturePreneurs 2023
+    <div className='m-10'>
+      <LeaderDashboard />
     </div>
   )
 }
