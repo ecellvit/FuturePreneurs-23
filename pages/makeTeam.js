09b93eacc0667eeeb9dd54ff64af843d4b72d895
Isha <ishushree19@gmail.com>
2023-10-09 20:10:15 +0530

    added styling to it

--- pages/makeTeam.js
+++ pages/makeTeam.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useHistory } from 'react-router-dom';
 
 const MakeTeam = () => {
   const [teamName, setTeamName] = useState('');
-  const router = useRouter();
+  const history = useHistory();
 
   const handleCreateTeam = async () => {
     try {
@@ -20,7 +20,7 @@ const MakeTeam = () => {
 
       if (data.isUnique) {
         // Team name is unique, so redirect to TeamCode page
-        router.push('/teamCode');
+        history.push('/TeamCode');
       } else {
         // Team name is already used, display an error message
         alert('Team name already used. Please choose a different name.');
@@ -32,30 +32,33 @@ const MakeTeam = () => {
 
   const handleJoinTeam = () => {
     // Redirect to JoinTeam page
-    router.push('/joinTeam');
+    history.push('/JoinTeam');
   };
 
   return (
-    <div className="card">
-      <div className="card-body">
-        <h1 className="card-title">Make a Team</h1>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="Enter Team Name"
-            value={teamName}
-            onChange={(e) => setTeamName(e.target.value)}
-          />
-        </div>
-        <div className="button-container">
-          <button className="create-button" onClick={handleCreateTeam}>
-            Create Team
-          </button>
-          <button className="join-button" onClick={handleJoinTeam}>
-            Join Team
-          </button>
-        </div>
+    <div className="border border-gray-300 rounded shadow-md p-4 bg-white max-w-xs mx-auto text-center">
+      <h1 className="text-2xl font-semibold mb-2">Make a Team</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Enter Team Name"
+          value={teamName}
+          onChange={(e) => setTeamName(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded"
+        />
       </div>
+      <button
+        className="px-4 py-2 rounded cursor-pointer bg-blue-500 text-white"
+        onClick={handleCreateTeam}
+      >
+        Create Team
+      </button>
+      <button
+        className="px-4 py-2 rounded cursor-pointer bg-green-500 text-white mt-4"
+        onClick={handleJoinTeam}
+      >
+        Join Team
+      </button>
     </div>
   );
 };
