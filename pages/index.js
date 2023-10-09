b637fec27cb8dfc7f8fe92105e6e7258b77543de
Pratyush <connect2pratyush@gmail.com>
2023-10-09 23:20:20 +0530

    signin

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
+
+  const {data : session} = useSession();
+  console.log("session", session);
+
   return (
     <div className='m-10'>
       FuturePreneurs 2023
+      <br />
+      {session ? <div> Signed In <button onClick={() => { signOut() }}>Sign Out</button> </div> :
+        <button onClick={() => { signIn('google') }}>Sign In with Google</button>
+      }
     </div>
   )
 }
