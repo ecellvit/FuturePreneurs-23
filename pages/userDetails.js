57b87add16a2ccf3e85301b526c849a12ad0fd1a
Pratyush <connect2pratyush@gmail.com>
2023-10-10 11:48:42 +0530

    warnings

--- pages/userDetails.js
+++ pages/userDetails.js
@@ -85,32 +85,32 @@ export default function userDetails()
         // alert('Registration successful!\nName: ' + name + '\nRegistration Number: ' + regNo + '\nPhone Number: ' + phone + '\nEmail: ' + email);
     }
     return(
-        <div class="container mx-auto p-4 mt-4">
-        <h1 class="text-2xl font-semibold mb-4">Your Personal Details Form</h1>
+        <div className="container mx-auto p-4 mt-4">
+        <h1 className="text-2xl font-semibold mb-4">Your Personal Details Form</h1>
 
-        <form id="registrationForm" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="User Name"></input>
-                <p id="nameError" class="text-red-500 text-xs italic hidden">Name already exists.</p>
+        <form id="registrationForm" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name</label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="User Name"></input>
+                <p id="nameError" className="text-red-500 text-xs italic hidden">Name already exists.</p>
             </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="reg_no">Registration Number</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="reg_no" type="text" placeholder="Registration Number"></input>
-                <p id="regNoError" class="text-red-500 text-xs italic hidden">Registration Number already exists.</p>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="reg_no">Registration Number</label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="reg_no" type="text" placeholder="Registration Number"></input>
+                <p id="regNoError" className="text-red-500 text-xs italic hidden">Registration Number already exists.</p>
             </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">Phone Number</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="Phone Number"></input>
-                <p id="phoneError" class="text-red-500 text-xs italic hidden">Phone Number already exists.</p>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">Phone Number</label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="Phone Number"></input>
+                <p id="phoneError" className="text-red-500 text-xs italic hidden">Phone Number already exists.</p>
             </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email ID"></input>
-                <p id="emailError" class="text-red-500 text-xs italic hidden">Email already exists.</p>
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email ID"></input>
+                <p id="emailError" className="text-red-500 text-xs italic hidden">Email already exists.</p>
             </div>
-            <div class="flex items-center justify-between">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onclick={validateRegistration()}>SUBMIT</button>
+            <div className="flex items-center justify-between">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onclick={validateRegistration()}>SUBMIT</button>
             </div>
         </form>
     </div>
