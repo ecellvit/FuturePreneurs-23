99e6d5716b9aee7bf38e054dde47859052bb2411
kv8gh <vyaskaran1409@gmail.com>
2023-12-29 23:26:03 +0530

    Landing_page(incomplete)

---
+++ Components/LandingPageComponent/LandingPageNavbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image';
+import logo from "public/assets/logos/fp logo.png";
+import eCellLogo from '@/public/assets/logos/ecell_logo.svg';
+import LoginButton from './Loginbutton';
+import Link from 'next/link';
+
+export default function LandingPageNavbar(){
+  return (
+    <nav className='flex text-white w-full justify-center h-[84px] py-4 m-4'>
+        <div className='flex justify-evenly items-center font-medium w-3/4'>
+            <Link href="/">HOME</Link>
+            <Link href="/">ABOUT</Link>
+            <Link href="/">TIMELINE</Link>
+            <Link href="/">CONTACT US</Link>
+            <LoginButton />
+            <Image src={eCellLogo} alt='eCellLogo' className='h-full w-auto'/>
+        </div>
+    </nav>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,36 @@ import { signIn, signOut, useSession } from 'next-auth/react';
 import eCellLogo from '@/public/assets/logos/ecell_logo.svg';
 import bg from "public/assets/bg/spceBg.svg";
 import Image from "next/image";
+import LandingPageNavbar from '@/components/LandingPageComponent/LandingPageNavbar';
+import LoginButton from '@/components/LandingPageComponent/Loginbutton';
+import GlobeAnimation from '@/components/LandingPageComponent/GlobeAnimation';
+import CountdownTimer from '@/components/LandingPageComponent/Timer';
+import { useEffect } from 'react';
+    
 
 export default function Home() {
-  const { data: session } = useSession();
-  console.log('session', session);
+  const targetDate = new Date(2024,0,1,0)
 
   return (
-    <main className="text-white">
-      <section className='Landing_page'>
+    <main className="text-white p-6 h-full">
+      <section className='Landing_page h-full'>
       <Image src={bg} fill className="object-cover z-[-10]" />
-      FuturePreneurs 2023
-      <Image src={eCellLogo} alt="eCellLogo" className="h-1/2" />
+      <GlobeAnimation />
+      <LandingPageNavbar />
+      
+      
+      <div className="mr-10 text-[#f4f4f4] font-['coalition'] font-black text-7xl">      
+        Future<br /><br /> 
+        Preneurs  9.0
+      </div>
       <br />
-      {session ? (
-        <div>
-          {' '}
-          Signed In{' '}
-          <button
-            onClick={() => {
-              signOut();
-            }}>
-            Sign Out
-          </button>{' '}
-        </div>
-      ) : (
-        <button
-          onClick={() => {
-            signIn('google', { callbackUrl: '/userDetails' });
-          }}>
-          Sign In with Google
-        </button>
-      )}
+      <CountdownTimer targetDate={targetDate}/>
+      <button className="rounded-full py-1 px-1 bg-gradient-to-br from-cyan-600 via-green-400 to-purple-600">
+      <div className="py-2 px-4 rounded-full bg-black">
+  Register!
+  </div>
+</button>
+
       </section>
     </main>
   );
