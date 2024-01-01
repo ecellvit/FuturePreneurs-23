d56b21de5654bc9c6a9a5ed1b88f8fbc59042e75
arjunbector <arjunbector@gmail.com>
2024-01-01 18:52:59 +0530

    add alt text to Images

--- Components/Landing Page/Footer.jsx
+++ Components/Landing Page/Footer.jsx
@@ -13,15 +13,15 @@ import x from "public/assets/landingPage/footer/socialMedia/x.svg"
 const Footer = () => {
   return (
     <footer className="relative h-fit flex flex-col sm:flex-row sm:px-14 items-center py-10 gap-5 sm:gap-0">
-        <Image src={bg} fill className="object-cover z-[-10]" />
+        <Image alt="backround space image" src={bg} fill className="object-cover z-[-10]" />
         <div className="w-full sm:w-1/3 h-full flex flex-col gap-5 sm:gap-10">
             <div className="flex flex-col items-center gap-3">
                 <h1 className="text-6xl font-bold">E-CELL</h1>
                 <h1>We Breed Business</h1>
             </div>
             <div className="flex flex-col items-center text-sm gap-2">
-                <div className="flex items-center"><Image src={phone} className="h-4"/><Link href={"tel:+91 6306311799"}>+91 6306311799</Link></div>
-                <div className="flex items-center"><Image src={mail} className="h-3"/><Link href={"mailto:helloecellvit@gmail.com"}>helloecellvit@gmail.com</Link></div>
+                <div className="flex items-center"><Image src={phone} alt="tel" className="h-4"/><Link href={"tel:+91 6306311799"}>+91 6306311799</Link></div>
+                <div className="flex items-center"><Image src={mail} alt="mail" className="h-3"/><Link href={"mailto:helloecellvit@gmail.com"}>helloecellvit@gmail.com</Link></div>
             </div>
         </div>
         <div className="w-full px-8">
@@ -29,10 +29,10 @@ const Footer = () => {
             <p className=" font-light text-sm tracking-wider">Entrepreneurship Cell is a Student based Club functioning under Office of Students' Welfare, VIT Vellore which aims at fostering entrepreneurial spirit amongst young aspirants by providing them with a platform and required resources for actuating their ideas into successful business ventures. E-Cell strives to attain an entrepreneurial environment in the campus and believes in taking strides towards establishing an ever-growing, ever-improving Start-Up environment. Our sole approach is to magnify the reach and set up a diverse pool of investors, evaluators and mentors.</p>
         </div>
         <div className="w-full sm:w-[10%] h-full flex sm:flex-col gap-3 items-center justify-center">
-            <Link target="blank" href={"https://www.facebook.com/ecellvit"}><Image className="h-10" src={fb}/></Link>
-            <Link target="blank" href={"https://www.instagram.com/ecell_vit"}><Image className="h-10" src={insta}/></Link>
-            <Link target="blank" href={"https://www.linkedin.com/company/ecellvitvellore"}><Image className="h-10" src={linkedin}/></Link>
-            <Link target="blank" href={"https://twitter.com/ecell_vit"}><Image className="h-10" src={x}/></Link>
+            <Link target="blank" href={"https://www.facebook.com/ecellvit"}><Image className="h-10" src={fb} alt="facebook icon"/></Link>
+            <Link target="blank" href={"https://www.instagram.com/ecell_vit"}><Image className="h-10" src={insta} alt="instagram icon"/></Link>
+            <Link target="blank" href={"https://www.linkedin.com/company/ecellvitvellore"}><Image className="h-10" src={linkedin} alt="linkedin icon"/></Link>
+            <Link target="blank" href={"https://twitter.com/ecell_vit"}><Image className="h-10" src={x} alt="twitter icon"/></Link>
         </div>
     </footer>
   )
--- Components/Landing Page/Timeline/TimelineCard.jsx
+++ Components/Landing Page/Timeline/TimelineCard.jsx
@@ -4,7 +4,7 @@ import img1 from "public/assets/landingPage/timeline/img1.svg";
 const TimelineCard = (props) => {
   return (
     <div className=" h-96 w-72 relative flex flex-col justify-between select-none">
-      <Image src={props.img} fill className="absolute z-[-5] object-cover"/>
+      <Image alt={props.text} src={props.img} fill className="absolute z-[-5] object-cover"/>
       <div className="p-2 text-lg font-bold">{props.id}</div>
       <div className="flex flex-col items-center text-center px-3">
         <h1 className="font-bold text-xl">
