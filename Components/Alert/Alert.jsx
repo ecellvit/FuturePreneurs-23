6bf88191ce5a1ad0d2a95f4221b8966708f16505
arjunbector <arjunbector@gmail.com>
2024-01-02 20:50:13 +0530

    change to font weight

--- Components/Alert/Alert.jsx
+++ Components/Alert/Alert.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 export default function Alert(props) {
   return (
     <motion.div
-      className="w-[50vw] bg-gray-800 text-green-400 p-2 rounded-lg absolute bottom-10 after:content-[''] after:top-0 after:left-0 after:w-full after:h-[1px]"
+      className="w-[50vw] bg-gray-800 text-green-400 font-medium p-2 rounded-lg absolute bottom-10 after:content-[''] after:top-0 after:left-0 after:w-full after:h-[1px]"
       initial={{
         translateY: "100%",
         opacity: 0,
