63ef1c8fd3b066836534679d509e3504f59ccad2
Pranav Murali <pranavmurali024@gmail.com>
2024-01-08 22:33:11 +0530

    Changed memberDashboard,leaderDashboard

--- Components/DeleteteamButton.jsx
+++ Components/DeleteteamButton.jsx
@@ -1,8 +1,12 @@
+// components/DeleteTeamButton.js
 import React from 'react';
 
 const DeleteTeamButton = ({ onClick }) => {
   return (
-    <button onClick={onClick} className="bg-red-500 text-white px-4 py-3 rounded-s-md">
+    <button
+      onClick={onClick}
+      className="bg-red-500 text-white px-3 py-1 border-0 rounded-none cursor-pointer"
+    >
       Delete Team
     </button>
   );
