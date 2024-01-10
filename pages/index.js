ef6a0b709a16d44015149518deb2be7f84ddc0eb
arjunbector <arjunbector@gmail.com>
2024-01-10 19:13:28 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -10,41 +10,48 @@ import StoryBehindSection from "@/Components/Landing Page/StoryBehindSection";
 import Ticker from "@/Components/Landing Page/Ticker";
 import Timeline from "@/Components/Landing Page/Timeline/Timeline";
 import CountdownTimer from '@/Components/Landing Page/Timer';
+import LoadingScreen from "@/Components/LoadingScreen";
 import styles from "@/styles/fpfont.module.css";
 import Image from "next/image";
 import bg from "public/assets/landingPage/bg.svg";
+import { useState } from "react";
 
 export default function Home() {
   const targetDate = new Date(2024,0,19,0,0)
+  const [isLoading, setIsLoading] = useState(false);
   return (
 
     <main className="text-white h-full">
-      <Image src={bg} alt="bgImage" fill className="object-cover z-[-10] w-full min-h-[100vh]" />
-      <section className='Landing_page p-2 relative h-[100svh] overflow-hidden'>
+      {isLoading ? <LoadingScreen/> : (<>
+        <Image src={bg} alt="bgImage" fill className="object-cover z-[-10] w-full min-h-[100vh]" />
+        <section className='Landing_page p-2 relative h-[100svh] overflow-hidden'>
+        
+        <GlobeAnimation />
+        <LandingPageNavbar />
+        
+        <div className="pl-5"> 
+        <div className={styles.fp}>     
+         <p>FUTURE</p>
+         <p>PRENEURS  9.0</p>
+         </div>
+         {/* <Image src={fpFont} alt="Future Preneurs Logo" />
+         <Image src={fpFont2} alt="9.0" /> */}
+        <CountdownTimer targetDate={targetDate}/>
+        <br/>
+        <RegisterButton text="Register!"/>
+        </div>
+        </section>
+        <BSG/>
+        <Timeline/>
+        <Ticker/>
+        <CardSection/>
+        <StoryBehindSection/>
+        <FAQ/>
+        <RegisterSection/>
+        <Footer/>
+        </>
+      )}
       
-      <GlobeAnimation />
-      <LandingPageNavbar />
-      
-      <div className="pl-5"> 
-      <div className={styles.fp}>     
-       <p>FUTURE</p>
-       <p>PRENEURS  9.0</p>
-       </div>
-       {/* <Image src={fpFont} alt="Future Preneurs Logo" />
-       <Image src={fpFont2} alt="9.0" /> */}
-      <CountdownTimer targetDate={targetDate}/>
-      <br/>
-      <RegisterButton text="Register!"/>
-      </div>
-      </section>
-      <BSG/>
-      <Timeline/>
-      <Ticker/>
-      <CardSection/>
-      <StoryBehindSection/>
-      <FAQ/>
-      <RegisterSection/>
-      <Footer/>
     </main>
   );
 }
--- pages/joinTeam/index.js
+++ pages/joinTeam/index.js
@@ -161,7 +161,7 @@ const JoinTeam = ({ teamCode: propTeamCode }) => {
             onChange={(e)=>{
               setTeamCode(e.target.value)
             }}
-            className="text-black border border-gray-300 dark:border-gray-600 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-500 dark:bg-gray-700"
+            className="text-white border border-gray-300 dark:border-gray-600 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-500 dark:bg-gray-700"
           />
         </div>
         <button
