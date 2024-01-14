51f9e2d7a6ec071c76e959971afbc5419bfcfe91
arjunbector <arjunbector@gmail.com>
2024-01-14 09:36:53 +0530

    add grid for level4

---
+++ pages/levels/level4.js
@@ -0,0 +1,19 @@
+export default function level4(){
+  return (
+    <main>
+      <div className="grid min-h-screen gap-1 grid-cols-5">
+        <div className=" h-full w-full bg-red-500 col-span-5 row-span-1">heading</div>
+        <div className="h-full w-full bg-red-500 row-span-4">problem statement</div>
+        <div className="h-full w-full bg-red-500 row-span-2">project/product name</div>
+        <div className="h-full w-full bg-red-500 row-span-4">key features</div>
+        <div className="h-full w-full bg-red-500 row-span-2">success criteria</div>
+        <div className="h-full w-full bg-red-500 row-span-4">Objective</div>
+        <div className="h-full w-full bg-red-500 row-span-2">target audience</div>
+        <div className="h-full w-full bg-red-500 row-span-2">testing plan</div>
+        <div className="h-full w-full bg-red-500 col-span-3">key metrices to measure success</div>
+        <div className="h-full w-full bg-red-500 col-span-2">constraints and guidlines</div>
+      </div>
+    </main>
+  )
+}
+
