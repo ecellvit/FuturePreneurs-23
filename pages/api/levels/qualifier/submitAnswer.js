23540c7f00f320e47ad9ab6307bd7a0cde9f0451
arjunbector <arjunbector@gmail.com>
2024-01-15 13:59:13 +0530

    submitAnswer route backend completed

--- pages/api/levels/qualifier/submitAnswer.js
+++ pages/api/levels/qualifier/submitAnswer.js
@@ -1,34 +1,43 @@
-import connectMongoDB from '@/libs/mongodb';
-import { Qualifier } from '@/models/qualifier';
-import { TeamModel } from '@/models/teamModel';
+import connectMongoDB from "@/libs/mongodb";
+import { Qualifier } from "@/models/qualifier";
+import { TeamModel } from "@/models/teamModel";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.status(405).json({ message: 'Method not allowed' });
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed" });
     return;
   } else {
     try {
       await connectMongoDB();
-      const teamName = 'team1';
+      const teamName = "team1";
 
-      const qualTeam = await Qualifier.findOne({ teamName: teamName })
+      const qualTeam = await Qualifier.findOne({ teamName: teamName });
       if (!qualTeam) {
-        res.status(400).json({ message: 'Team not found' })
-        return
+        res.status(400).json({ message: "Team not found" });
+        return;
       }
 
-      // const secondQual = await Qualifier.findAndUpdate({ teamName: teamName }, { : req.body.answer })
-
-      res.status(200).json({ message: 'Answer submitted' });
-
-    } catch (e) {
-      console.log(e);
-      res
-        .status(500)
-        .json({
-          message: 'Internal server error',
-          error: e.toString(),
-        });
+      res.status(200).json({ message: "Answer submitted" });
+      const answerData = req.body;
+      console.log("answerData", answerData);
+      const teamData = await Qualifier.findOne({ teamName: teamName });
+      const questionPointer = teamData.questionPointer;
+      console.log(questionPointer);
+      const userAnswers = teamData.userAnswers;
+      userAnswers[questionPointer] = answerData;
+      await Qualifier.updateOne(
+        { teamName: teamName },
+        { userAnswers: userAnswers,
+          questionPointer: questionPointer + 1 }
+      );
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({
+        message: "Internal server error",
+        error: e.toString(),
+      });
     }
   }
 }
+
+// 10 easy, 8 med, 4 hard, 1cs
