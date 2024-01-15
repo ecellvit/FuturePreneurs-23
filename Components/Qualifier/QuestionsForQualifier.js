85c26137ad5083f665347bc3f02cf9f9440fb740
kv8gh <vyaskaran1409@gmail.com>
2024-01-15 23:32:55 +0530

    caseStudypdf

--- Components/Qualifier/QuestionsForQualifier.js
+++ Components/Qualifier/QuestionsForQualifier.js
@@ -204,66 +204,15 @@ export default function QuestionForQualifier(props) {
         {props.questionCategory === "caseStudy" && (
           <div className="flex flex-col text-xl w-3/4 h-fit p-5 border border-black rounded-lg">
             <div>Question {props.questionNumber + 23}</div>
+            <iframe src="/assets/levels/navbar/qualifier/pdf.pdf#toolbar=0&navpanes=0" />
             <div>
               {
                 questions[props.questionCategory][props.questionNumber].q
                   .content
               }
             </div>
-            {questions[props.questionCategory][props.questionNumber].q
-              .contentType === "image" && (
-              <img
-                src={
-                  questions[props.questionCategory][props.questionNumber].q
-                    .contentLink
-                }
-              />
-            )}
-            {questions[props.questionCategory][props.questionNumber].q
-              .contentType === "audio" && (
-              <div>
-                {typeof(
-                  questions[props.questionCategory][props.questionNumber].q
-                    .contentLink
-                ) === "object" ? (
-                  <div>
-                    <audio controls className="mb-2">
-                      <source
-                        src={
-                          questions[props.questionCategory][
-                            props.questionNumber
-                          ].q.contentLink[0]
-                        }
-                        type="audio/mpeg"
-                      />
-                      Your browser does not support the audio element.
-                    </audio>
-                    <audio controls className="mb-2">
-                      <source
-                        src={
-                          questions[props.questionCategory][
-                            props.questionNumber
-                          ].q.contentLink[1]
-                        }
-                        type="audio/mpeg"
-                      />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </div>
-                ) : (
-                  <audio controls className="mb-2">
-                    <source
-                      src={
-                        questions[props.questionCategory][props.questionNumber]
-                          .q.contentLink
-                      }
-                      type="audio/mpeg"
-                    />
-                    Your browser does not support the audio element.
-                  </audio>
-                )}
-              </div>
-            )}
+            
+            
           </div>
         )}
       </section>
