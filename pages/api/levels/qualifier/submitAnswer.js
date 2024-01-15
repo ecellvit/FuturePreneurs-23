733d5716c2a669b13532a902a545f44198491625
arjunbector <arjunbector@gmail.com>
2024-01-15 15:18:44 +0530

    backend for submitAnswers

--- pages/api/levels/qualifier/submitAnswer.js
+++ pages/api/levels/qualifier/submitAnswer.js
@@ -1,6 +1,5 @@
 import connectMongoDB from "@/libs/mongodb";
 import { Qualifier } from "@/models/qualifier";
-import { TeamModel } from "@/models/teamModel";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -17,27 +16,70 @@ export default async function handler(req, res) {
         return;
       }
 
-      res.status(200).json({ message: "Answer submitted" });
-      const answerData = req.body;
+      const answerData = req.body.answer;
       console.log("answerData", answerData);
       const teamData = await Qualifier.findOne({ teamName: teamName });
       const questionPointer = teamData.questionPointer;
+      const easyOrder = teamData.easyOrder;
+      const mediumOrder = teamData.mediumOrder;
+      const hardOrder = teamData.hardOrder;
+      const easyAnswers = teamData.easyAnswers;
+      const mediumAnswers = teamData.mediumAnswers;
+      const hardAnswers = teamData.hardAnswers;
+      const caseStudyAnswers = teamData.caseStudyAnswers;
+
+      let questionCategory = teamData.questionCategory;
+      let newQuestionPointer = questionPointer;
+
+      if (questionCategory === "easy") {
+        easyAnswers[easyOrder[questionPointer]] = answerData;
+      } else if (questionCategory == "medium") {
+        mediumAnswers[mediumOrder[questionPointer]] = answerData;
+      } else if (questionCategory == "hard") {
+        hardAnswers[hardOrder[questionPointer]] = answerData;
+      } else if (questionCategory == "caseStudy") {
+        caseStudyAnswers[questionPointer] = answerData;
+      }
+
+      if (questionCategory === "easy" && questionPointer === 9) {
+        newQuestionPointer = 0;
+        questionCategory = "medium";
+      } else if (questionCategory === "medium" && questionPointer === 7) {
+        newQuestionPointer = 0;
+        questionCategory = "hard";
+      } else if (questionCategory === "hard" && questionPointer === 3) {
+        newQuestionPointer = 0;
+        questionCategory = "caseStudy";
+      } else if (questionCategory === "caseStudy" && questionPointer === 3) {
+        newQuestionPointer = 0;
+        questionCategory = "waiting";
+      } else if (questionCategory == "waiting") {
+        return res.status(400).json({ message: "Qualifier round is completed." });
+      } else {
+        newQuestionPointer = questionPointer + 1;
+      }
+
       console.log(questionPointer);
-      const userAnswers = teamData.userAnswers;
-      userAnswers[questionPointer] = answerData;
       await Qualifier.updateOne(
         { teamName: teamName },
-        { userAnswers: userAnswers,
-          questionPointer: questionPointer + 1 }
+        {
+          questionPointer: newQuestionPointer,
+          questionCategory: questionCategory,
+          easyAnswers: easyAnswers,
+          mediumAnswers: mediumAnswers,
+          hardAnswers: hardAnswers,
+          caseStudyAnswers: caseStudyAnswers,
+        }
       );
+      return res.status(200).json({ message: "Answer submitted" });
     } catch (err) {
       console.log(err);
       return res.status(500).json({
         message: "Internal server error",
-        error: e.toString(),
+        error: err.toString(),
       });
     }
   }
 }
 
-// 10 easy, 8 med, 4 hard, 1cs
+// 10 easy, 8 med, 4 hard, 4cs
