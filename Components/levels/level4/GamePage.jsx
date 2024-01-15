c0ce9e01c2c74b37e5f9ac431c02352dc87d8d4c
Pratyush <connect2pratyush@gmail.com>
2024-01-15 16:22:19 +0530

    asdf

--- Components/levels/level4/GamePage.jsx
+++ Components/levels/level4/GamePage.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import Navbar from "../Navbar";
+import { useState } from 'react';
+import Navbar from '../Navbar';
 
 export default function GamePage() {
   const [formData, setFormData] = useState({
-    problemStatement: "",
-    projectName: "",
-    keyFeatures: "",
-    successCriteria: "",
-    objective: "",
-    targetAudience: "",
-    testingPlan: "",
-    successMetrics: "",
-    constraintsGuidelines: "",
+    problemStatement: '',
+    projectName: '',
+    keyFeatures: '',
+    successCriteria: '',
+    objective: '',
+    targetAudience: '',
+    testingPlan: '',
+    successMetrics: '',
+    constraintsGuidelines: '',
   });
 
   const handleChange = (e) => {
@@ -27,23 +27,23 @@ export default function GamePage() {
 
     try {
       // Send formData to the backend using a fetch or Axios
-      const response = await fetch("/api/levels/level4/sendData", {
-        method: "POST",
+      const response = await fetch('/api/levels/level4/sendData', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
       console.log(formData);
       if (response.status === 200) {
         // Handle success, maybe show a success message to the user
-        console.log("Data successfully sent to the backend.");
+        console.log('Data successfully sent to the backend.');
       } else {
         // Handle errors, maybe show an error message to the user
-        console.error("Failed to send data to the backend.");
+        console.error('Failed to send data to the backend.');
       }
     } catch (error) {
-      console.error("Error:", error.message);
+      console.error('Error:', error.message);
     }
   };
 
@@ -51,11 +51,10 @@ export default function GamePage() {
     <main
       className=" bg-cover bg-no-repeat bg-center text-white text-center w-full"
       style={{
-        backgroundImage: "url(/assets/bg/spceBg.svg)",
-        minHeight: "100vh",
-      }}
-    >
-    <Navbar sendData={""} level="level4"/>
+        backgroundImage: 'url(/assets/bg/spceBg.svg)',
+        minHeight: '100vh',
+      }}>
+      <Navbar sendData={()=>{handleSubmit()}} level="level4" />
       <form onSubmit={handleSubmit}>
         <div className="grid min-h-screen gap-3 grid-cols-5 px-10 pt-10">
           <div className="h-full w-full flex items-center justify-center text-3xl leading-none bg-gradient-to-r from-[#416B93] to-[#0F4474] font-bold col-span-5 row-span-1">
@@ -86,7 +85,9 @@ export default function GamePage() {
             />
           </div>
           <div className="h-full w-full bg-gradient-to-r from-[#416B93] to-[#0F4474] font-semibold row-span-4 flex flex-col ">
-            <h1 className="font-semibold text-xl uppercase">Key Features</h1>
+            <h1 className="font-semibold text-xl uppercase">
+              Key Features
+            </h1>
             <textarea
               className="w-full h-full bg-transparent text-white p-2 resize-none"
               name="keyFeatures"
@@ -106,7 +107,9 @@ export default function GamePage() {
             />
           </div>
           <div className="h-full w-full bg-gradient-to-r from-[#416B93] to-[#0F4474] font-semibold row-span-4 flex flex-col ">
-            <h1 className="font-semibold text-xl uppercase">Objective</h1>
+            <h1 className="font-semibold text-xl uppercase">
+              Objective
+            </h1>
             <textarea
               className="w-full h-full bg-transparent text-white p-2 resize-none"
               name="objective"
@@ -115,7 +118,9 @@ export default function GamePage() {
             />
           </div>
           <div className="h-full w-full bg-gradient-to-r from-[#416B93] to-[#0F4474] font-semibold row-span-2 flex flex-col">
-            <h1 className="font-semibold text-xl uppercase">target audience</h1>
+            <h1 className="font-semibold text-xl uppercase">
+              target audience
+            </h1>
             <textarea
               className="w-full h-full bg-transparent text-white p-2 resize-none"
               name="targetAudience"
@@ -124,7 +129,9 @@ export default function GamePage() {
             />
           </div>
           <div className="h-full w-full bg-gradient-to-r from-[#416B93] to-[#0F4474] font-semibold row-span-2 flex flex-col ">
-            <h1 className="font-semibold text-xl uppercase">testing plan</h1>
+            <h1 className="font-semibold text-xl uppercase">
+              testing plan
+            </h1>
             <textarea
               className="w-full h-full bg-transparent text-white p-2 resize-none"
               name="testingPlan"
@@ -160,8 +167,7 @@ export default function GamePage() {
         <div className="flex justify-end w-full py-4 px-10">
           <button
             type="submit"
-            className="rounded-full cursor-pointer bg-gradient-to-r from-[#416B93] to-[#0F4474] w-fit px-6 mb-5 h-12 flex items-center justify-center font-bold"
-          >
+            className="rounded-full cursor-pointer bg-gradient-to-r from-[#416B93] to-[#0F4474] w-fit px-6 mb-5 h-12 flex items-center justify-center font-bold">
             Submit
           </button>
         </div>
