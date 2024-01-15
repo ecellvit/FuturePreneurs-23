414076daf0fd82601c53c442909b89da0e500def
smritidoneria <smritidoneria@gmail.com>
2024-01-15 16:58:12 +0530

    updated merge team route and fetchdata routes

---
+++ pages/api/admin/f73kdnv675ien/fetchdata3.js
@@ -0,0 +1,35 @@
+
+import { TeamModel1 } from "@/models/test";
+import { Users1 } from "@/models/testuser";
+import connectMongoDB from '@/libs/mongodb';
+import { Level3test } from "@/models/level3test";
+
+export default async function handler(req, res) {
+    try {
+        connectMongoDB()
+        const teams=await TeamModel1.find();
+        let team;
+        for(team of teams){
+            const teamId=team._id;
+            const teamName=team.teamName;
+            const leaderName=team.leaderName;
+            const leaderEmail=team.leaderEmail;
+            const newLevel1 = await new Level3test({
+                teamName: teamName,
+                teamId: teamId,
+                leaderName:leaderName,
+                leaderEmail:leaderEmail
+            }).save();
+
+        }
+        res.status(200).json({
+            message:"Data has been assigned"
+        })
+
+      
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
+
